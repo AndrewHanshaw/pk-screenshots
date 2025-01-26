6443d4a392426552a40043b70aff6391d8acde20
Drew Hanshaw <drew.hanshaw@cnhind.com>
2025-01-26 16:27:40 -0600

    Add empty MacOS XCode project

---
+++ pkScreenshotsTests/pkScreenshotsTests.swift
@@ -0,0 +1,17 @@
+//
+//  pkScreenshotsTests.swift
+//  pkScreenshotsTests
+//
+//  Created by Andrew Hanshaw on 1/26/25.
+//
+
+import Testing
+@testable import pkScreenshots
+
+struct pkScreenshotsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
