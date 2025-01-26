e5d9731a1953259f3d8d4e9bfa6be19b1baca203
Drew Hanshaw <drew.hanshaw@cnhind.com>
2025-01-26 16:27:40 -0600

    Add empty MacOS XCode project

---
+++ pkScreenshots/pkScreenshotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  pkScreenshotsApp.swift
+//  pkScreenshots
+//
+//  Created by Andrew Hanshaw on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct pkScreenshotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
